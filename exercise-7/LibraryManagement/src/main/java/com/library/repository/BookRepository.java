9e764abb49a6400c87edf9f2d286efedf100a2f4
Yoshitha26 <132035434+Yoshitha26@users.noreply.github.com>
2024-08-08 17:30:35 +0530

    Delete exercise-7 directory

--- exercise-7/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        // Simulate saving a book
-        System.out.println("Save book: " + bookName);
-    }
-}
